d0d20694062f564deae0588cd4822659ab98e18e
Alp Mestanogullari <alp.mestanogullari@tweag.io>
2015-11-26 13:34:28 +0100

    bring in hs-invoke(-bin) in the simple-spark-app dir

---
+++ simple-spark-app/hs-invoke/Simple.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE StaticPointers #-}
+module Simple where
+
+import Control.Distributed.Closure
+import Spark
+
+import qualified Data.ByteString      as BS
+
+f :: Int -> Int
+f x = x * 2
+
+wrappedF :: BS.ByteString -> BS.ByteString
+wrappedF = wrap1 f
+
+fClosure :: Closure (BS.ByteString -> BS.ByteString)
+fClosure = closure (static wrappedF)
