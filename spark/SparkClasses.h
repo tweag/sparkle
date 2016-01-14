ff90a372e962157e580d8473864dc6db089530a2
Alp Mestanogullari <alp.mestanogullari@tweag.io>
2016-01-14 20:45:43 +0100

    wip support for Spark's parallelize operation

--- spark/SparkClasses.h
+++ spark/SparkClasses.h
@@ -2,3 +2,4 @@
 
 jobject newSparkContext(jobject sparkConf);
 jobject newSparkConf(const char* appname);
+jobject parallelize(jobject sparkContext, int* data, size_t data_length);
--- spark/src/Spark.hs
+++ spark/src/Spark.hs
@@ -4,10 +4,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Spark where
 
+import           Control.Distributed.Closure
+-- import           Data.Binary.Serialise.CBOR
 import qualified Data.ByteString as BS
 import           Data.ByteString (ByteString)
 import           Data.Monoid     ((<>))
+import           Data.Vector     (Vector, fromList)
 import           Foreign.C.String (withCString)
+import           Foreign.C.Types
+import           Foreign.Marshal.Array (withArrayLen)
 import           JNI
 import qualified Language.C.Inline as C
 
@@ -15,24 +20,53 @@ C.context (C.baseCtx <> jniCtx)
 
 C.include "../SparkClasses.h"
 
+-- TODO:
+-- eventually turn all the 'type's into 'newtype's.
+
 -- withCString :: String -> (CString -> IO a) -> IO a
 
-newSparkConf :: String -> IO JObject
+type SparkConf = JObject
+
+newSparkConf :: String -> IO SparkConf
 newSparkConf name =
     withCString name $ \nameptr -> do
       [C.block| jobject {
            newSparkConf($(char *nameptr));
       } |]
 
-newSparkContext :: JObject -> IO JObject
+type SparkContext = JObject
+
+newSparkContext :: SparkConf -> IO SparkContext
 newSparkContext conf =
     [C.block| jobject {
          newSparkContext($(jobject conf));
     } |]
 
+type RDD = JObject
+
+parallelize :: SparkContext -> [CInt] -> IO RDD
+parallelize sc vec = withArrayLen vec $ \vecLen vecBuf ->
+  let vecLen' = fromIntegral vecLen in
+  [C.block| jobject {
+      parallelize($(jobject sc), $(int* vecBuf), $(size_t vecLen'));
+  } |]
+
+rddmap :: Closure (Int -> Int)
+       -> RDD
+       -> IO RDD
+rddmap = undefined
+
+collect :: RDD -> IO (Vector Int)
+collect = undefined
+
 sparkMain :: IO ()
 sparkMain = do
     conf <- newSparkConf "Hello sparkle!"
-    print =<< newSparkContext conf
+    sc   <- newSparkContext conf
+    rdd  <- parallelize sc [1..10]
+    -- rdd' <- rddmap f rdd'
+    -- res  <- collect rdd'
+    -- print res
+    return ()
 
 foreign export ccall sparkMain :: IO ()
