cfaf8b724d3c7deebd1e27cd16e5f18ad5cea1e3
Alp Mestanogullari <alp.mestanogullari@tweag.io>
2016-01-19 15:13:22 +0100

    refactor C code for getting our hands on Java methods

--- spark/SparkClasses.h
+++ spark/SparkClasses.h
@@ -2,12 +2,14 @@
 
 JNIEnv* jniEnv();
 
-jclass  findClass(const char* java_class);
-jobject newObject(const char* java_class, const char* sig, const jvalue* args);
-jstring newString(const char* str);
+jclass    findClass(const char* java_class);
+jmethodID findMethod(jclass java_class, const char* method_name, const char* sig);
+jmethodID findStaticMethod(jclass java_class, const char* method_name, const char* sig);
+jobject   newObject(jclass java_class, const char* sig, const jvalue* args);
+jstring   newString(const char* str);
 
-jobject newSparkContext(jobject sparkConf);
-jobject newSparkConf(const char* appname);
-jobject parallelize(jobject sparkContext, jint* data, size_t data_length);
-void    collect(jobject rdd, int** buf, size_t* len);
-jobject rddmap(jobject rdd, char* clos, long closSize);
+jobject   newSparkContext(jobject sparkConf);
+jobject   newSparkConf(const char* appname);
+jobject   parallelize(jobject sparkContext, jint* data, size_t data_length);
+void      collect(jobject rdd, int** buf, size_t* len);
+jobject   rddmap(jobject rdd, char* clos, long closSize);
