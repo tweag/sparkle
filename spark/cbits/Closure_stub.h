7b697775dd13607c51c83541168473e575ff86e0
Alp Mestanogullari <alp.mestanogullari@tweag.io>
2016-02-08 22:10:49 +0100

    working filter() & count() demo

--- spark/cbits/Closure_stub.h
+++ spark/cbits/Closure_stub.h
@@ -2,7 +2,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern HsPtr invokeC(HsPtr a1, HsInt64 a2, HsPtr a3);
+extern HsBool invokeC(HsPtr a1, HsInt64 a2, HsPtr a3, HsInt64 a4);
 #ifdef __cplusplus
 }
 #endif
