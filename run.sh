e3d468b36512dca5c0a45bf330e903b6bcafbd87
Mathieu Boespflug <m@tweag.io>
2016-02-11 12:34:32 +0100

    Don't hardcode bash path.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 stack exec spark-submit -- --class SparkMain --driver-library-path .:$(./findLibDir.sh "$1") --master local[1] examples/target/sparkle-1.0-jar-with-dependencies.jar
