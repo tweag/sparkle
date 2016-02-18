59f626ce35cae377952dbd8615aac9b0eaf0d77e
Mathieu Boespflug <m@tweag.io>
2016-02-18 12:35:01 +0100

    Make hello example build again.

--- examples/hello/HelloSpark.hs
+++ examples/hello/HelloSpark.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StaticPointers #-}
 
 module HelloSpark where
@@ -5,25 +6,29 @@ module HelloSpark where
 import Control.Distributed.Closure
 import Control.Distributed.Spark as RDD
 import Foreign.Java
-import Data.List (isInfixOf)
+import qualified Data.Text as Text
+import Data.Text (Text)
 
-f1 :: String -> Bool
-f1 s = "a" `isInfixOf` s
+f1 :: Text -> Bool
+f1 s = "a" `Text.isInfixOf` s
 
-f2 :: String -> Bool
-f2 s = "b" `isInfixOf` s
+f2 :: Text -> Bool
+f2 s = "b" `Text.isInfixOf` s
 
-sparkMain :: JVM -> IO ()
-sparkMain jvm = do
-    env  <- jniEnv jvm
+sparkMain :: JNIEnv -> JClass -> IO ()
+sparkMain env _ = do
+    attach env
     conf <- newSparkConf env "Hello sparkle!"
     sc   <- newSparkContext env conf
     rdd  <- textFile env sc "stack.yaml"
-    as   <- RDD.filter env (closure $ static f1) rdd
-    bs   <- RDD.filter env (closure $ static f2) rdd
+    as   <- RDD.filter env (closure (static f1)) rdd
+    bs   <- RDD.filter env (closure (static f2)) rdd
     numAs <- RDD.count env as
     numBs <- RDD.count env bs
     putStrLn $ show numAs ++ " lines with a, "
             ++ show numBs ++ " lines with b."
 
-foreign export ccall sparkMain :: JVM -> IO ()
+foreign export ccall "Java_io_tweag_sparkle_Sparkle_sparkMain" sparkMain
+  :: JNIEnv
+  -> JClass
+  -> IO ()
