ad3a0d34d85fdc8a01b2da53d6bcfd5e56bc1213
Mathieu Boespflug <m@tweag.io>
2016-02-18 12:33:14 +0100

    Import HaskellFunction* .java source files.
    This is used as the target classes for reflecting Haskell functions.

---
+++ sparkle/src/main/java/io/tweag/sparkle/function/HaskellFlatMapFunction.java
@@ -0,0 +1,16 @@
+package io.tweag.sparkle.function;
+
+import org.apache.spark.api.java.function.*;
+import io.tweag.sparkle.Sparkle;
+
+public class HaskellFlatMapFunction<T, R> implements FlatMapFunction<T, R> {
+    private final byte[] clos;
+
+    public HaskellFlatMapFunction(final byte[] clos) {
+	this.clos = clos;
+    }
+
+    public Iterable<R> call(T value) throws Exception {
+	return Sparkle.apply(clos, value);
+    }
+}
---
+++ sparkle/src/main/java/io/tweag/sparkle/function/HaskellFunction2.java
@@ -0,0 +1,16 @@
+package io.tweag.sparkle.function;
+
+import org.apache.spark.api.java.function.*;
+import io.tweag.sparkle.Sparkle;
+
+public class HaskellFunction2<T1, T2, R> implements Function2<T1, T2, R> {
+    private final byte[] clos;
+
+    public HaskellFunction2(final byte[] clos) {
+	this.clos = clos;
+    }
+
+    public R call(T1 v1, T2 v2) throws Exception {
+	return Sparkle.apply(clos, v1, v2);
+    }
+}
