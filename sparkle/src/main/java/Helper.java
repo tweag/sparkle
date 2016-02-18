b8f57a90c2320d83c960c5b6701f989394264bda
Mathieu Boespflug <m@tweag.io>
2016-02-18 09:00:24 +0100

    Remove Java helper functions that are no longer needed.

--- sparkle/src/main/java/Helper.java
+++ sparkle/src/main/java/Helper.java
@@ -15,41 +15,6 @@ import scala.Tuple2;
 
 public class Helper
 {
-    public static JavaRDD<Integer> parallelize
-	(JavaSparkContext sc, int[] data)
-    {
-	Integer[] newArray = new Integer[data.length];
-
-	for(int i = 0; i < data.length; i++)
-	{
-	    newArray[i] = Integer.valueOf(data[i]);
-	}
-
-	List<Integer> arg = new ArrayList<Integer>(Arrays.asList(newArray));
-	JavaRDD<Integer> rdd = sc.parallelize(arg);
-        return rdd;
-    }
-
-    public static int[] collect(JavaRDD<Integer> rdd)
-    {
-	List<Integer> l = rdd.collect();
-	int[] res = new int[l.size()];
-	for(int i = 0; i < l.size(); i++)
-	    res[i] = l.get(i).intValue();
-	return res;
-    }
-
-    public static JavaPairRDD<Long, String> swapPairs(JavaPairRDD<String, Long> prdd)
-    {
-	return prdd.mapToPair(
-	    new PairFunction<Tuple2<String, Long>, Long, String>() {
-		public Tuple2<Long, String> call(Tuple2<String, Long> t)
-		{
-		    return new Tuple2<Long, String>(t._2(), t._1());
-		}
-	    });
-    }
-
     public static RegexTokenizer setupTokenizer(RegexTokenizer rt, String icol, String ocol, boolean gaps, String patt)
     {
 	return rt.setInputCol(icol)
@@ -59,23 +24,12 @@ public class Helper
 	         .setMinTokenLength(5);
     }
 
-    public static JavaRDD<Integer> map(JavaRDD<Integer> rdd, final byte[] clos)
+    public static JavaRDD<Row> toRows(JavaPairRDD<String, Long> prdd)
     {
-	JavaRDD<Integer> newRDD = rdd.map(new Function<Integer, Integer>() {
-		public Integer call(Integer arg)
+	JavaRDD<Row> res = prdd.map(new Function<Tuple2<String, Long>, Row>() {
+		public Row call(Tuple2<String, Long> tup)
 		{
-		    return new Integer(HaskellRTS.invoke(clos, arg.intValue()));
-		}
-	});
-	return newRDD;
-    }
-
-    public static JavaRDD<Row> toRows(JavaPairRDD<Long, String> prdd)
-    {
-	JavaRDD<Row> res = prdd.map(new Function<Tuple2<Long, String>, Row>() {
-		public Row call(Tuple2<Long, String> tup)
-		{
-		    return RowFactory.create(tup._1(), tup._2());
+		    return RowFactory.create(tup._2(), tup._1());
 		}
 	});
 	return res;
