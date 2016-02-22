ec5adbb69971ce0601febbccb45c11c352ec9880
Mathieu Boespflug <m@tweag.io>
2016-02-22 12:24:33 +0100

    Package dynamically linked executables dependencies in .jar.
    We arrange for maven to discover all dynamically linked libraries that
    the Haskell app depends on, and stick 'em in the jar. For simplicity,
    we encapsulate all the discovered libs in a .zip file that then goes
    into the JAR. This gives us an easy manifest of items to unpack at
    runtime, which we do with some Java code, but we could consider other
    approaches in the future.

---
+++ sparkle/copy-exe.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR=$(${STACK_EXE:-stack} path --local-install-root)
+TARGET_DIR=$(mktemp -d)
+for i in $(ldd $DIR/bin/$1 | awk '{print $3}')
+do
+    cp $i $TARGET_DIR
+done
+cp $DIR/bin/$1 $TARGET_DIR/hsapp
+(cd $TARGET_DIR; zip app *)
+mv $TARGET_DIR/app.zip $2
+rm -rf $TARGET_DIR
