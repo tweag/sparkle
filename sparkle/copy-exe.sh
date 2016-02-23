e6432657c024e20e0db344eb954e5c096d1b5b36
Mathieu Boespflug <m@tweag.io>
2016-02-23 10:14:01 +0100

    Auto create target dir.

--- sparkle/copy-exe.sh
+++ sparkle/copy-exe.sh
@@ -6,7 +6,7 @@ for i in $(ldd $DIR/bin/$1 | awk '{print $3}')
 do
     cp $i $TARGET_DIR
 done
-cp $DIR/bin/$1 $TARGET_DIR/hsapp
+install -D $DIR/bin/$1 $TARGET_DIR/hsapp
 (cd $TARGET_DIR; zip app *)
 
 [ -d $2 ] || mkdir $2 # ensures that the resources/ dir exists
