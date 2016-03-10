049d94164449e3893eb58f637584d4ae9f215d2b
Alp Mestanogullari <alpmestan@gmail.com>
2016-03-10 10:34:26 +0100

    wip osx support

--- sparkle/cbits/bootstrap.c
+++ sparkle/cbits/bootstrap.c
@@ -8,6 +8,7 @@ extern int main(int argc, char *argv[]);
 
 JavaVM *sparkle_jvm;
 
+/*
 pthread_spinlock_t sparkle_init_lock;
 static int sparkle_initialized;
 
@@ -15,6 +16,7 @@ __attribute__((constructor)) void sparkle_init_lock_constructor()
 {
 	pthread_spin_init(&sparkle_init_lock, 0);
 }
+*/
 
 /* Ensure that global variables are initialized. */
 static void sparkle_init(JNIEnv *env, int init_rts)
@@ -23,16 +25,16 @@ static void sparkle_init(JNIEnv *env, int init_rts)
 	char *argv[] = { NULL }; /* or e.g { "+RTS", "-A1G", "-H1G", NULL }; */
 	char **pargv = argv;
 
-	pthread_spin_lock(&sparkle_init_lock);
-	if(!sparkle_initialized) {
-		if(init_rts)
+	// pthread_spin_lock(&sparkle_init_lock);
+	// if(!sparkle_initialized) {
+	//	   if(init_rts)
 			hs_init(&argc, &pargv);
 		/* Store the current JVM in a global. The current JNI spec
 		 * (2016) supports only one JVM per address space anyways. */
 		(*env)->GetJavaVM(env, &sparkle_jvm);
-		sparkle_initialized = 1;
-	}
-	pthread_spin_unlock(&sparkle_init_lock);
+	// 	  sparkle_initialized = 1;
+	// }
+	// pthread_spin_unlock(&sparkle_init_lock);
 }
 
 JNIEXPORT jobject JNICALL Java_io_tweag_sparkle_Sparkle_apply
