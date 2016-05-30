62d6aa1d62f966c12792cfbf68b87b196363b275
Alp Mestanogullari <alp.mestanogullari@tweag.io>
2016-05-30 15:03:06 +0200

    Use `getOrCreate` to get a SparkContext.
    This is the required interface on Databricks Spark.

--- apps/hello/HelloSpark.hs
+++ apps/hello/HelloSpark.hs
@@ -19,7 +19,7 @@ main = do
     conf <- newSparkConf "Hello sparkle!"
     confSet conf "spark.hadoop.fs.s3n.awsAccessKeyId" "AKIAIKSKH5DRWT5OPMSA"
     confSet conf "spark.hadoop.fs.s3n.awsSecretAccessKey" "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"
-    sc   <- newSparkContext conf
+    sc   <- getOrCreate conf
     rdd  <- textFile sc "s3n://tweag-sparkle/lorem-ipsum.txt"
     as   <- RDD.filter (closure (static f1)) rdd
     bs   <- RDD.filter (closure (static f2)) rdd
