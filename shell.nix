c3e801643d578e610345be48956dd6f4acba6715
Mathieu Boespflug <m@tweag.io>
2016-09-11 17:23:38 +0200

    Remove obsolete code in shell.nix.
    Now that upstream nixpkgs has been updated.

--- shell.nix
+++ shell.nix
@@ -1,59 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {}, ghc ? nixpkgs.haskell.compiler.ghc7103 }:
+{ nixpkgs ? import <nixpkgs> {}, ghc }:
 
 with nixpkgs;
-with stdenv.lib;
 
 let
   spark = nixpkgs.spark.override { mesosSupport = false; };
   openjdk = openjdk7;
-
   jvmlibdir =
     if stdenv.isLinux
     then "${openjdk}/lib/openjdk/jre/lib/amd64/server"
     else "${openjdk}/jre/lib/server";
-
-  # TODO: remove once https://github.com/NixOS/nixpkgs/pull/15989 is merged.
-  buildStackProject =
-    { buildInputs ? []
-    , extraArgs ? []
-    , LD_LIBRARY_PATH ? ""
-    , ghc ? ghc
-    , ...
-    }@args:
-
-    stdenv.mkDerivation (args // {
-
-    buildInputs =
-      buildInputs ++
-      optional stdenv.isLinux glibcLocales ++
-      [ ghc pkgconfig ];
-
-    STACK_IN_NIX_SHELL=1;
-    STACK_IN_NIX_EXTRA_ARGS =
-      args.STACK_IN_NIX_EXTRA_ARGS or
-      concatMap (pkg: ["--extra-lib-dirs=${pkg}/lib"
-                       "--extra-include-dirs=${pkg}/include"]) buildInputs ++
-      extraArgs;
-
-    # XXX: workaround for https://ghc.haskell.org/trac/ghc/ticket/11042.
-    LD_LIBRARY_PATH = makeLibraryPath (LD_LIBRARY_PATH ++ buildInputs);
-
-    preferLocalBuild = true;
-
-    configurePhase = args.configurePhase or "stack setup";
-
-    buildPhase = args.buildPhase or "stack build";
-
-    checkPhase = args.checkPhase or "stack test";
-
-    doCheck = args.doCheck or true;
-
-    installPhase = args.installPhase or ''
-      stack --local-bin-path=$out/bin build --copy-bins
-    '';
-    });
 in
-buildStackProject {
+haskell.lib.buildStackProject {
   name = "sparkle";
   buildInputs =
     [ gradle
