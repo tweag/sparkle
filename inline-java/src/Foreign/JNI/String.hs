700f6bc1c8810ae74af98dd6eb3d64ae8c0c4900
Mathieu Boespflug <m@tweag.io>
2016-10-02 13:32:35 +0200

    Use bytestrings as the backing store for JNI.String.
    It's now a wrapper around bytestrings, rather than manipulating
    ForeignPtr's directly. This turns out to make the implementation quite
    a bit shorter, and also avoids any problems with creating a new
    foreign pointer to the same buffer as a bytestring. That is a recipe
    for disaster: the bytestring's finalizer will eventually free the
    buffer, and we'll be left high and dry pointing to unallocated memory.

--- inline-java/src/Foreign/JNI/String.hs
+++ inline-java/src/Foreign/JNI/String.hs
@@ -25,31 +25,15 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Unsafe as BS
 import Data.ByteString (ByteString)
 import Data.String (IsString(..))
-import Foreign.C.Types (CChar)
-import Foreign.ForeignPtr
-  ( ForeignPtr
-  , newForeignPtr
-  , newForeignPtr_
-  , withForeignPtr
-  )
-import Foreign.Marshal.Alloc (finalizerFree)
+import Foreign.C.String (CString)
 import qualified GHC.Foreign as GHC
 import qualified GHC.IO.Encoding as GHC
-import GHC.Ptr (Ptr(..))
 import System.IO.Unsafe (unsafeDupablePerformIO)
 import qualified Prelude
 import Prelude hiding (String)
 
-newtype String = String (ForeignPtr CChar)
-
-foreign import ccall unsafe "string.h strcmp" strcmp :: Ptr CChar -> Ptr CChar -> Int
-
-instance Eq String where
-  String fptr1 == String fptr2 =
-    unsafeDupablePerformIO $
-    withForeignPtr fptr1 $ \x ->
-    withForeignPtr fptr2 $ \y ->
-    return $ strcmp x y == 0
+newtype String = String ByteString
+  deriving (Eq)
 
 instance Show String where
   show str = show (toChars str)
@@ -60,31 +44,20 @@ instance IsString String where
 fromChars :: Prelude.String -> String
 {-# INLINE [0] fromChars #-}
 fromChars str = unsafeDupablePerformIO $ do
-    cstr <- GHC.newCString GHC.utf8 str
-    String <$> newForeignPtr finalizerFree cstr
+    String <$> (BS.unsafePackCString =<< GHC.newCString GHC.utf8 str)
 
 toChars :: String -> Prelude.String
-toChars (String fptr) =
-    unsafeDupablePerformIO $ withForeignPtr fptr $ GHC.peekCString GHC.utf8
+toChars (String bs) = unsafeDupablePerformIO $ BS.unsafeUseAsCString bs $ GHC.peekCString GHC.utf8
 
-withString :: String -> (Ptr CChar -> IO a) -> IO a
-withString (String fptr) action = withForeignPtr fptr action
+withString :: String -> (CString -> IO a) -> IO a
+withString (String bs) f = BS.unsafeUseAsCString bs f
 
 toByteString :: String -> ByteString
-toByteString (String fptr) =
-    unsafeDupablePerformIO $
-      withForeignPtr fptr $
-        BS.unsafePackCString
+toByteString (String bs) = bs
 
 -- | O(1) if the input is null-terminated. Otherwise the input is copied into
 -- a null-terminated buffer first.
 fromByteString :: ByteString -> String
 fromByteString bs
-  | BS.last bs == 0 = convert bs
-  | otherwise = convert (bs `BS.snoc` 0)
-  where
-    convert bs1 =
-      unsafeDupablePerformIO $
-        -- No finalizer, because we're reusing the ByteString's buffer, which
-        -- already has one (don't want to double free).
-        String <$> BS.unsafeUseAsCString bs1 newForeignPtr_
+  | BS.last bs == 0 = String bs
+  | otherwise = String (bs `BS.snoc` 0)
