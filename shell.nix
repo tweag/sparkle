635e43f8a86b8a896c4b9447d0f91cd1b8d1ad55
Facundo Domínguez <facundo.dominguez@tweag.io>
2017-10-24 09:39:48 -0300

    Pin nixpkgs to a specific version.

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 , ghc ? nixpkgs.haskell.compiler.ghc802 # Default needed for Docker build.
 }:
 
-with nixpkgs;
+with import (fetchTarball https://github.com/nixos/nixpkgs/archive/e91840cfb6b7778f8c29d455a2f24cffa1b4e43e.tar.gz) {};
 
 let
   spark = nixpkgs.spark.override { mesosSupport = false; };
