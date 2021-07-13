d5137658922ff8ed0c08d1592929ed1dd3d1fba9
Noah Goodman <ngoodman@uchicago.edu>
2021-07-13 18:10:25 -0400

    removed dependence from s3 for memory error script and added more options

--- apps/memerr/Main.hs
+++ apps/memerr/Main.hs
@@ -1,40 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StaticPointers #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 module Main where
 
 import Control.Distributed.Closure
 import Control.Distributed.Spark as RDD
 import Control.Monad
+import Data.Choice 
 import Data.IORef
-import Data.Maybe
 -- import qualified Data.Text as Text
--- import Data.Text (Text)
-import System.Environment
-import Text.Read
+import Data.Text (Text)
+import Options.Applicative as Opt
 
+argsParser :: Parser (Int, Choice "useIORef")
+argsParser = (,) <$> option auto (value 100 <> Opt.short 'n' <> metavar "N")
+                 <*> flag (Do #useIORef) (Don't #useIORef) (Opt.long "no-io-ref")
 
 main :: IO ()
 main = forwardUnhandledExceptionsToSpark $ do
     conf <- newSparkConf "Memory memery"
+    {-
     confSet conf "spark.hadoop.fs.s3a.aws.credentials.provider"
                  "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider"
+                 -}
     -- NOTE: it would be ideal if we could just set the driver memory here
     -- by dynamically modifying the spark config, but when run in local mode,
     -- you have to set it through the `spark-submit` CLI
     sc   <- getOrCreateSparkContext conf
-    -- Figure out how many references we want to create 
-    numRefs <- fromMaybe 100 . (readMaybe <=< listToMaybe) <$> getArgs
+    -- Figure out how many references we want to create and whether to use
+    -- iorefs
+    -- NOTE: 200,000 is about the amount the we need to get the program to crash
+    (numRefs, useIORef) <- execParser (info argsParser fullDesc)
+    -- numRefs <- fromMaybe 100 . (readMaybe <=< listToMaybe) <$> getArgs
     print numRefs
-    -- This S3 bucket is located in US East.
-    rdd  <- textFile sc "s3a://tweag-sparkle/lorem-ipsum.txt"
+    rdd  <- parallelize sc ["yes", "no", "maybe"]
     -- Create an ioref to hold on to all the references we create
     ior <- newIORef []
-    forM_ [0..numRefs:: Int] $ \_ -> do
-      rdd' <- RDD.map (closure $ static (<> "hello")) rdd
-      modifyIORef ior (rdd' :)
+    -- Perform the main loop, optionally using ioref to store refs to rdd
+    forM_ [0 .. numRefs :: Int] $ \_ -> do
+      rdd' <- RDD.map (closure $ static (id @Text)) rdd
+      Control.Monad.when (toBool useIORef) $ modifyIORef ior (rdd' :)
     --
-    len  <- RDD.count rdd 
-    putStrLn $ show len ++ " lines total."
     refs <- readIORef ior
     putStrLn $ "ref list length: " ++ show (Prelude.length refs)
