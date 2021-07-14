63f70e29425331aae202003cea6ff13bf2c256e0
Noah Goodman <ngoodman@uchicago.edu>
2021-07-14 15:35:36 -0400

    cleaned up memerr app a bit

--- apps/memerr/Main.hs
+++ apps/memerr/Main.hs
@@ -12,10 +12,17 @@ import Control.Distributed.Spark as RDD
 import Control.Monad
 import Data.Choice 
 import Data.IORef
--- import qualified Data.Text as Text
 import Data.Text (Text)
 import Options.Applicative as Opt
 
+-- Parser for the command line options
+--
+-- the -n option controls how many references to create 
+-- NOTE: 200,000 is about the number we need to crash the JVM with the 
+-- driver memory set to 512M
+--
+-- and if the --no-io-ref flag is present, we will not store references in an
+-- ioref, so the GHC garbage collecter will have a chance to collect them
 argsParser :: Parser (Int, Choice "useIORef")
 argsParser = (,) <$> option auto (value 100 <> Opt.short 'n' <> metavar "N")
                  <*> flag (Do #useIORef) (Don't #useIORef) (Opt.long "no-io-ref")
@@ -23,20 +30,12 @@ argsParser = (,) <$> option auto (value 100 <> Opt.short 'n' <> metavar "N")
 main :: IO ()
 main = forwardUnhandledExceptionsToSpark $ do
     conf <- newSparkConf "Memory memery"
-    {-
-    confSet conf "spark.hadoop.fs.s3a.aws.credentials.provider"
-                 "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider"
-                 -}
     -- NOTE: it would be ideal if we could just set the driver memory here
     -- by dynamically modifying the spark config, but when run in local mode,
     -- you have to set it through the `spark-submit` CLI
     sc   <- getOrCreateSparkContext conf
-    -- Figure out how many references we want to create and whether to use
-    -- iorefs
-    -- NOTE: 200,000 is about the amount the we need to get the program to crash
     (numRefs, useIORef) <- execParser (info argsParser fullDesc)
-    -- numRefs <- fromMaybe 100 . (readMaybe <=< listToMaybe) <$> getArgs
-    print numRefs
+    putStrLn $ "# of references to be created: " ++ show numRefs
     rdd  <- parallelize sc ["yes", "no", "maybe"]
     -- Create an ioref to hold on to all the references we create
     ior <- newIORef []
